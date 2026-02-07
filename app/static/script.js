beff6e449e5b50b18ebb13e5b8c204a337231733
wagnermarzzo <marzzowagner@gmail.com>
2026-02-07 20:36:20 -0300

    Add sendResult function to handle fetch requests

---
+++ app/static/script.js
@@ -0,0 +1,10 @@
+function sendResult(result) {
+  fetch(`/round?result=${result}`, { method: "POST" })
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("signal").innerText = "Sinal: " + data.signal;
+      document.getElementById("confidence").innerText = "Confiança: " + data.confidence + "%";
+      document.getElementById("greens").innerText = data.greens;
+      document.getElementById("reds").innerText = data.reds;
+    });
+          }
