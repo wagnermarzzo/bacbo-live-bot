5aeed8de194e55cc830005058cd4514447969e94
wagnermarzzo <marzzowagner@gmail.com>
2026-02-07 19:25:54 -0300

    Delete app directory

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.engine.analyzer import process_round
-
-app = FastAPI(title="BacBo Live Analyzer")
-
-@app.post("/round")
-def new_round(result: str):
-    return process_round(result)
